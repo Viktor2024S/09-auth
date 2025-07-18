ea5b59fc11b82eb4952cd3077a117232412745db
Viktor Momotenko <viktor.momotenko@gmail.com>
2025-07-18 03:31:38 +0100

    header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,3 @@
-// components/Header/Header.tsx
 "use client";
 
 import Link from "next/link";
@@ -14,25 +13,17 @@ const AppNavigationBar = () => {
 
   return (
     <header className={headerStyles.header}>
-      {/* Use .container to wrap content for flex layout */}
       <div className={headerStyles.container}>
-        {/* Apply .logo class for the NoteHub link */}
         <Link href="/" aria-label="Home" className={headerStyles.logo}>
           NoteHub
         </Link>
 
         <nav aria-label="Main Navigation" className={headerStyles.navigation}>
-          {/* Apply .navigation class to the <ul> element for list styling */}
           <ul className={headerStyles.navigation}>
             {" "}
-            {/* Use .navigation for the ul to apply list-style-type: none etc. */}
             <li className={headerStyles.navigationItem}>
               {" "}
-              {/* Assuming .navigationItem will be added to CSS */}
-              <Link
-                href={dynamicHomeLink}
-                className={headerStyles.link} // Apply .link class
-              >
+              <Link href={dynamicHomeLink} className={headerStyles.link}>
                 Home
               </Link>
             </li>
@@ -41,10 +32,7 @@ const AppNavigationBar = () => {
                 <NoteTagsDropdownMenu />
               </li>
             )}
-            {/* AuthenticationNavbar likely contains its own styling, ensure it works with .navigationItem or adjust */}
             <li className={headerStyles.navigationItem}>
-              {" "}
-              {/* Wrap AuthenticationNavbar in an li if it represents a nav item */}
               <AuthenticationNavbar />
             </li>
           </ul>
